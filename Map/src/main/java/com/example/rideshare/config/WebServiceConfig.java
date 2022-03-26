562922b996c01402568563877770a5a517f54197
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-03-27 01:11:23 +0600

    added wsdl for customer payment header

--- Map/src/main/java/com/example/rideshare/config/WebServiceConfig.java
+++ Map/src/main/java/com/example/rideshare/config/WebServiceConfig.java
@@ -28,12 +28,12 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean(name = "countries")
     public Wsdl11Definition defaultWsdl11Definition() {
         SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
-        wsdl11Definition.setWsdl(new ClassPathResource("/contracts/countries.wsdl"));
+        wsdl11Definition.setWsdl(new ClassPathResource("/contracts/customer.wsdl"));
         return wsdl11Definition;
     }
 
 //    @Bean
 //    public XsdSchema countriesSchema() {
-//        return new SimpleXsdSchema(new ClassPathResource("schemas/countries.xsd"));
+//        return new SimpleXsdSchema(new ClassPathResource("schemas/customer.xsd"));
 //    }
 }
