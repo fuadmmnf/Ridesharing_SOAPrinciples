980d9d4956c175b4df08d48d3b0cc60720386215
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-03-28 09:41:26 +0600

    payment service coded

--- Payment/src/main/java/com/example/rideshare/endpoints/PaymentEndpoint.java
+++ Payment/src/main/java/com/example/rideshare/endpoints/PaymentEndpoint.java
@@ -4,7 +4,10 @@ package com.example.rideshare.endpoints;
 
 //import com.example.rideshare.gen.CountryRequest;
 //import com.example.rideshare.gen.CountryResponse;
+import com.example.rideshare.gen.AcknowledgementCode;
+import com.example.rideshare.gen.AcknowledgementCodeResponse;
 import com.example.rideshare.repositories.PaymentRepository;
+import generated.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -15,19 +18,41 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 public class PaymentEndpoint {
     private static final String NAMESPACE_URI = "http://www.example.com/rideshare/gen";
 
-    private PaymentRepository countryRepository;
+    private PaymentRepository paymentRepository;
 
     @Autowired
     public PaymentEndpoint(PaymentRepository countryRepository) {
-        this.countryRepository = countryRepository;
+        this.paymentRepository = countryRepository;
     }
 
-//    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "CountryRequest")
-//    @ResponsePayload
-//    public CountryResponse getCountry(@RequestPayload CountryRequest request) {
-//        CountryResponse response = new CountryResponse();
-//        response.setCountry(countryRepository.findCountry(request.getName()));
-//
-//        return response;
-//    }
+    private AcknowledgementCodeResponse createAckResponse(AcknowledgementCode code) {
+        AcknowledgementCodeResponse codeResponse = new AcknowledgementCodeResponse();
+        codeResponse.setAcknowledgementcode(code);
+        return codeResponse;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "InitiatePaymentRequest")
+    @ResponsePayload
+    public AcknowledgementCodeResponse initiatePayment(@RequestPayload InitiatePaymentRequest initiatePaymentRequest) {
+        return this.createAckResponse(AcknowledgementCode.CREATED);
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "HandlePaymentSuccessRequest")
+    @ResponsePayload
+    public AcknowledgementCodeResponse handlePaymentSuccess(@RequestPayload HandlePaymentSuccessRequest handlePaymentSuccessRequest) {
+        return this.createAckResponse(AcknowledgementCode.UPDATED);
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "HandlePaymentFailureRequest")
+    @ResponsePayload
+    public AcknowledgementCodeResponse handlePaymentFailure(@RequestPayload HandlePaymentFailureRequest handlePaymentFailureRequest) {
+        return this.createAckResponse(AcknowledgementCode.UPDATED);
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetTransactionsRequest")
+    @ResponsePayload
+    public GetTransactionsResponse getTransactions(@RequestPayload GetTransactionsRequest getTransactionsRequest) {
+        return new GetTransactionsResponse();
+    }
+
 }
